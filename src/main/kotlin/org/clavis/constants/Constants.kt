c8ee81f5a3cad599613d4d6a497d7234390d8d45
rahulranafz <rahul.ranafz@hotmail.com>
2017-11-28 17:02:15 +0530

    commit to add login functionality test

--- src/main/kotlin/org/clavis/constants/Constants.kt
+++ src/main/kotlin/org/clavis/constants/Constants.kt
@@ -2,6 +2,6 @@ package org.clavis.constants
 
 class Constants {
     //config
-    val CONFIG_WEB_FILE_PATH = "src/config/configData.properties"
+    val CONFIG_WEB_FILE_PATH = "src/main/kotlin/org/clavis/config/config.properties"
 
 }
